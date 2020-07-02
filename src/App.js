e790c247744839ff2eeb381f14c0897362fb51d6
Nathan <nathan.dunning@nzrb.co.nz>
2020-07-02 17:54:51 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// Default Import
+import React from 'react';
+
+// Pages
+import Home from './components/main/Home';
+
+// CSS
+import './components/main/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  //Reducers here: Name
+});
+
+export default rootReducer;
